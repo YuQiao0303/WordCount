2ab5e2eeb29fd48530fdfd30df2b93bbd7991723
Yu Qiao <691761071@qq.com>
2018-03-20 17:47:59 +0800

    test: change the content of test.bat

--- BIN/test.bat
+++ BIN/test.bat
@@ -1,17 +1,12 @@
-wc.exe -s -c -a -l -w *.c -e stoplist.txt -o output.txt
-wc.exe -s -w *.c 
+wc.exe -s -c -a -w *.c -e stoplist.txt -o output.txt
+wc.exe -s -l sub1\*.c 
 wc.exe -a input1.c
+
 wc.exe -c input1.c  
-  
 wc.exe -w input1.c 
 wc.exe -l input1.c 
 wc.exe -c input1.c -o output.txt
 wc.exe -a input1.c
 wc.exe -l -s *.c
 wc.exe -w input1.c -e stoplist.txt
-
-
-	
-
-
 
